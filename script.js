8ad11c0bd207b261f4a191bd9129c2f9ff402350
rlacycon <156925352+rlacycon@users.noreply.github.com>
2024-04-26 22:50:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -9,12 +9,15 @@ document.getElementById("mortgageForm").addEventListener("submit", function(even
     document.getElementById("result").innerHTML = "<p>Your monthly payment will be $" + monthlyPayment.toFixed(2) + "</p>";
 });
 
-// researched how to use Math.pow() from W3Schools as my previous implementation was not working
+// researched how to use Math. from W3Schools as my previous implementation was not working
 
 function calculateMonthlyPayment(loanAmount, interestRate, loanTerm) {
     var monthlyInterestRate = interestRate / 100 / 12;
     var numberOfPayments = loanTerm * 12;
     var monthlyPayment = (loanAmount * monthlyInterestRate) / 
     (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
+    
+    monthlyPayment = Math.round(monthlyPayment);
+
     return monthlyPayment;
 }
